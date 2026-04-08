83b08f579edcc518d660e0a74995f25aca8009b3
arjun-30 <arjunrr2004@gmail.com>
2026-04-09 01:39:01 +0530

    updated UI / feature

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAtN91ruu8qBIcnU7vV0LPuBL2Jr-zKisg",
+  authDomain: "roundu-workspace.firebaseapp.com",
+  projectId: "roundu-workspace",
+  storageBucket: "roundu-workspace.firebasestorage.app",
+  messagingSenderId: "981633793399",
+  appId: "1:981633793399:web:a8337c484a5d9c1174b572",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
